66176616d083d25b9298aed2ce8ce845bcfbfa3f
Annu Benny <annubenny101@gmail.com>
2020-06-16 12:52:29 -0400

    updated

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -4,10 +4,13 @@
 
 # Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
 # Task 2: Change the output to only show:
-# assigning 3 values to three variables
-firstnum=5
-secondnum=2
-thirdnum=3
+# using the echo command, reading values from the user
+echo -n "Enter the first number: "
+read firstnum
+echo -n "Enter the second number: "
+read secondnum
+echo -n "Enter the third number: "
+read thirdnum
 
 #  the sum of the 3 numbers with a label
 sum=$(( firstnum + secondnum + thirdnum))
