230209e99c846ec5ebbb8fe8695f48da6b73f13e
Annu Benny <annubenny101@gmail.com>
2020-06-25 01:32:38 -0400

    lab 3 first one

---
+++ bash/login-welcome.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+
+###############
+# Variables   #
+###############
+title="Overlord"
+USER="annu"
+hostname=$(hostname)
+
+#commands to find the day and time
+echo "CLOCK"
+echo "######"
+
+
+#command to find the day
+day=$(date +%A)
+#command to find the hour
+hour=$(date +%I)
+#command to find the minute
+min=$(date +%M)
+#command to display whether its AM/PM
+meridiem=$(date +%P)
+
+echo "It is $day at $hour:$min $meridiem"
+
+#to display each days with title, we use if elif commands
+
+if [ $day == "Monday" ]
+then
+  title="Day One"
+
+elif [ $day == "Tuesday" ]
+then
+   title="Day Two"
+elif [ $day == "Wednesday" ]
+then
+    title="Day Three"
+elif [ $day == "Thursday" ]
+then
+     title="Day Four"
+elif [ $day == "Friday" ]
+then
+      title="Day Five"
+elif [ $day == "Saturday" ]
+then
+      title="Day Six"
+else
+      title="Day Seven"
+fi
+
+#creating a new variable called finaloutput
+#storing the output to this variable
+
+finaloutput="Welcome to planet $hostname,   $title $USER!"
+#displaying an image of cow pointing the output
+cowsay $finaloutput
