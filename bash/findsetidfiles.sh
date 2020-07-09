29d9fcc540b2528adbdf71ff389e692e651ad974
Annu Benny <annubenny101@gmail.com>
2020-07-09 13:39:46 -0400

    findsetidfiles.sh

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Setgid files"
-echo"=============="
+echo "=============="
 # command to find and list the files according to the permission sorted with type and date
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 4
 
@@ -9,5 +9,5 @@ find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 4
 echo "Listing 10 Largest Files"
 echo "=========================="
 # using the find command, listing files by their sizes
-find / -type f -exec ls -l --block-size=MB {} + 2>/dev/null | sort -k5 -hr |head -n 10 |awk '{print $3,$5,$10}'
+find / -type f -exec ls -l --block-size=MB {} + 2>/dev/null | sort -k5 -hr |head -n 10 | awk '{print $3,$5,$9}' 
 echo ""
